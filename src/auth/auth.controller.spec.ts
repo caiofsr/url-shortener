dfff15f2278c162a85847f1d58550f03107d28ea
Caio Fernando <caiofsr@proton.me>
2024-08-19 17:03:50 -0300

    add tests to signup endpoint

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,38 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestBed } from '@automock/jest';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: jest.Mocked<AuthService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-    }).compile();
+    const { unit, unitRef } = TestBed.create(AuthController).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    controller = unit;
+    authService = unitRef.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create user', async () => {
+    const createUserDto = {
+      email: 'testing@example.com',
+      password: 'Testing@123',
+    };
+
+    authService.createUser = jest.fn().mockImplementation(() => {
+      return {
+        email: createUserDto.email,
+        id: 1,
+      };
+    });
+
+    expect(await controller.signup(createUserDto)).toEqual({
+      email: createUserDto.email,
+      id: 1,
+    });
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,41 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestBed } from '@automock/jest';
 import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let prismaService: jest.Mocked<PrismaService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    const { unit, unitRef } = TestBed.create(AuthService).compile();
 
-    service = module.get<AuthService>(AuthService);
+    service = unit;
+    prismaService = unitRef.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create user', async () => {
+    const createUserDto = {
+      email: 'testing@example.com',
+      password: 'Testing@123',
+    };
+
+    prismaService.user.create = jest.fn().mockImplementation(() => {
+      return {
+        email: createUserDto.email,
+        id: 1,
+      };
+    });
+
+    const user = await service.createUser({
+      email: createUserDto.email,
+      password: createUserDto.password,
+    });
+
+    expect(user).toEqual({ email: createUserDto.email, id: 1 });
+    expect(prismaService.user.create).toHaveBeenCalledTimes(1);
   });
 });
